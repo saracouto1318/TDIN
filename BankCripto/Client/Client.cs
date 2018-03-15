8453d6f75e5dc2bbd1ed885c3cef436321c8a61b
VascoUP <vasco_mag_pereira@hotmail.com>
2018-03-15 18:05:53 +0000

    First commit

---
+++ BankCripto/Client/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Client starting soon");
+        RemotingConfiguration.Configure("Client.exe.config", false);
+        RemObj obj = new RemObj();
+        Console.WriteLine(obj.Hello());
+    }
+}
+
+class RemObj : MarshalByRefObject
+{
+    public RemObj()
+    {
+        Console.WriteLine("Constructor called");
+    }
+
+    public string Hello()
+    {
+        return null;
+    }
+}
---
+++ BankCripto/Server/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+class Server
+{
+    static void Main()
+    {
+        Console.WriteLine("Server starting soon");
+        RemotingConfiguration.Configure("Server.exe.config", false);
+        Console.WriteLine("Press return to exit");
+        Console.ReadLine();
+    }
+}
+
+class RemObj : MarshalByRefObject
+{
+    public string Hello()
+    {
+        return "Hello user :)";
+    }
+}
