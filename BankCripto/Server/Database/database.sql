25feb7ad78c86ed9b97b020b9e1ff9fefb46a741
saracouto1318 <saracouto1318@hotmail.com>
2018-03-26 21:24:03 +0100

    Database added

--- BankCripto/Server/Database/database.sql
+++ BankCripto/Server/Database/database.sql
@@ -1,25 +1,24 @@
 CREATE TABLE User(
     nickname varchar(50) NOT NULL PRIMARY KEY,
-    firstName varchar(100) NOT NULL,
-    lastName varchar(100) NOT NULL,
+    name varchar(500) NOT NULL,
     password varchar(100) NOT NULL
 );
 
 CREATE TABLE Value(
-    power real NOT NULL
+    power real NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Diginote(
-    serialNumber int NOT NULL PRIMARY KEY,
-    onwer varchar(50) NOT NULL,
+    serialNumber INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    owner varchar(50) NOT NULL,
     facialValue real NOT NULL DEFAULT(1),
-    FOREIGN KEY (onwer) REFERENCES User(nickname)
+    FOREIGN KEY (owner) REFERENCES User(nickname)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
 CREATE TABLE TransactionDiginote(
-    transactionID int NOT NULL PRIMARY KEY,
+    transactionID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     diginoteID int NOT NULL,
     seller varchar(50) NOT NULL,
     buyer varchar(50) NOT NULL,
